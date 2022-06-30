a4c74b0d1aee21e10d740089e00243c39cc290b7
MMundanu <mmundanu@yahoo.com>
2022-06-30 18:56:31 -0300

    home hecho

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 3030;
 app.use(express.static("public"));
 app.listen(port, console.log("server running in http://localhost:" + port));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-app.get("/contact", (req, res) => res.sendFile(path.join(__dirname, "contact.html")));
-app.get("/music", (req, res) => res.sendFile(path.join(__dirname, "music.html")));
-app.get("/about", (req, res) => res.sendFile(path.join(__dirname, "about.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/views/index.html")));
+app.get("/contact", (req, res) => res.sendFile(path.join(__dirname, "./views/contact.html")));
+app.get("/music", (req, res) => res.sendFile(path.join(__dirname, "./views/music.html")));
+app.get("/about", (req, res) => res.sendFile(path.join(__dirname, "./views/about.html")));
 
 
